a7fc2d5ea9a89b75e6d619c3290ffb5c1e5dcfaa
cvelez <carolina.velez@mercadolibre.com.co>
2023-05-01 23:44:57 -0500

    agregar el formulario para pagar

---
+++ app/src/main/java/com/application/apptienda/ui/buy/DialogPaymentMethod.kt
@@ -0,0 +1,30 @@
+package com.application.apptienda.ui.buy
+
+import android.app.Dialog
+import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.DialogFragment
+import com.application.apptienda.R
+
+class DialogPaymentMethod: DialogFragment() {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog = Dialog(requireContext())
+
+        val dialogView = layoutInflater.inflate(R.layout.dialog_payment_method, null)
+
+        val btnBuy: Button = dialogView.findViewById(R.id.btn_buy)
+        btnBuy.setOnClickListener{
+            // Iniciar el checkout de Mercado Pago
+
+            val buyFragment = BuyFragment()
+            val transaction = parentFragmentManager.beginTransaction()
+            transaction.replace(R.id.nav_host_fragment_content_main, buyFragment)
+            transaction.addToBackStack(null)
+            transaction.commit()
+            dismiss()
+        }
+        dialog.setContentView(dialogView)
+        return dialog
+    }
+}
